d896164d5774e41dd310dc6069fe3827455094bf
许飞 <feixu@zhejianglab.org>
2025-02-28 15:32:55 +0800

    fix

--- baselines/oss/oss.py
+++ baselines/oss/oss.py
@@ -8,18 +8,12 @@ from io import BufferedWriter, BytesIO, BufferedReader
 # 配置日志
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# 检查环境变量是否已设置
-required_env_vars = ['OSS_ACCESS_KEY_ID', 'OSS_ACCESS_KEY_SECRET']
-for var in required_env_vars:
-    if var not in os.environ:
-        logging.error(f"Environment variable {var} is not set.")
-        exit(1)
-
 # 设置Endpoint和Region
 endpoint = "http://oss-cn-hangzhou-zjy-d01-a.ops.cloud.zhejianglab.com/"
 region = "cn-hangzhou"
 
-auth = oss2.Auth(os.getenv("OSS_ACCESS_KEY_ID"), os.getenv("OSS_ACCESS_KEY_SECRET"))
+# auth = oss2.Auth(os.getenv("OSS_ACCESS_KEY_ID"), os.getenv("OSS_ACCESS_KEY_SECRET"))
+auth = oss2.Auth("D3XdW8uT66mdJ3TO", "LpBPv841EkOhqNV8tiaknQfWJJm1uP")
 
 
 def Bucket(name) -> oss2.Bucket:
