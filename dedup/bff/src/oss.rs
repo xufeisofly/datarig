f06163db9b91c6bac565194b3f9ef8c088076ce5
许飞 <feixu@zhejianglab.org>
2025-03-05 15:44:00 +0800

    fix

--- dedup/bff/src/oss.rs
+++ dedup/bff/src/oss.rs
@@ -6,7 +6,7 @@ pub fn get_bucket(bucket_name: String) -> OSS<'static> {
     // 否则，新建并插入到缓存中
     let access_id = env::var("OSS_ACCESS_KEY_ID").unwrap();
     let access_secret = env::var("OSS_ACCESS_KEY_SECRET").unwrap();
-    println!("access_id: {access_id}, access_secret: {access_secret}");
+    // println!("access_id: {access_id}, access_secret: {access_secret}");
 
     let endpoint = "http://oss-cn-hangzhou-zjy-d01-a.ops.cloud.zhejianglab.com/";
     let new_oss = OSS::new(access_id, access_secret, endpoint.into(), bucket_name);
