61bced0014ea34293acbda87fbd0b127b34a08b1
许飞 <feixu@zhejianglab.org>
2025-03-05 10:06:33 +0800

    fix

--- dedup/bff/src/oss.rs
+++ dedup/bff/src/oss.rs
@@ -4,6 +4,7 @@ use std::env;
 pub fn get_bucket(bucket_name: String) -> OSS<'static> {
     let access_id = env::var("OSS_ACCESS_KEY_ID").unwrap();
     let access_secret = env::var("OSS_ACCESS_KEY_SECRET").unwrap();
+    println!("access_id: {access_id}, access_secret: {access_secret}");
     let endpoint = "http://oss-cn-hangzhou-zjy-d01-a.ops.cloud.zhejianglab.com/".into();
     let oss_instance = OSS::new(access_id, access_secret, endpoint, bucket_name);
     return oss_instance;
