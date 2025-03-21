e91c7c927326e218b1702fdf724f19fa551dcb2c
许飞 <feixu@zhejianglab.org>
2025-03-21 17:52:06 +0800

    fix

--- ray_processing/process.py
+++ ray_processing/process.py
@@ -236,7 +236,7 @@ def get_task_item(retry_tasks=False, task_file_path=DEFAULT_TASKS_FILE_PATH, loc
                             asigned_task['file_range'],
                             is_temp=asigned_task['is_temp'],
                             files=asigned_task['files'],
-                            original_shard_dir=asigned_task['original_shard_dir'])
+                            original_shard_dir=asigned_task.get('original_shard_dir', None))
         except BaseException as e:
             print(f"get task item failed: {e}")
             lock.release()
