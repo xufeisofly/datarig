3e8360cb5d1dd1e6e1a4341f5249daf73093a5a7
许飞 <feixu@zhejianglab.org>
2025-03-21 12:55:55 +0800

    fix

--- scripts/upload_files_to_oss.py
+++ scripts/upload_files_to_oss.py
@@ -10,6 +10,7 @@ def upload_file_to_oss(file_path, to_dir, bucket):
     上传单个文件到 OSS，file_path 为本地文件的完整路径，
     to_dir 为 OSS 中目标目录，bucket 为 OSS Bucket 实例。
     """
+    print("==== start upload file: {}".format(file_path))
     file_name = os.path.basename(file_path)
     oss_file_path = os.path.join(to_dir, file_name)
     _, oss_file_path = oss.split_file_path(oss_file_path)
