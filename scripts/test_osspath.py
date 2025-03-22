fdbe32aae602fb84c32d48d7d56c907b7edf4d21
许飞 <feixu@zhejianglab.org>
2025-03-23 00:28:50 +0800

    fix

--- scripts/test_osspath.py
+++ scripts/test_osspath.py
@@ -4,15 +4,22 @@ import json
 
 from baselines.oss import oss
 from baselines.core.file_utils import read_jsonl
+from ray_processing.process import finished_task_file
 
 
 if __name__ == '__main__':
     file_path = "oss://si002558te8h/dclm/process_tasks.json"
+    fin_file_path = finished_task_file(file_path)
 
     with open("./process_tasks.json", "w") as localf:
         with oss.OSSPath(file_path).open("r") as f:
             data = json.loads(f.read())
             json.dump(data, localf, indent=4)
+
+    with open("./finished_process_tasks.json", "w") as localf:
+        with oss.OSSPath(fin_file_path).open("r") as f:
+            data = json.loads(f.read())
+            json.dump(data, localf, indent=4)    
     
     
     # list(read_jsonl(file_path))
