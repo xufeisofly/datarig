1cec8a129b02158a8c092e7f3abca487c6b9875a
许飞 <feixu@zhejianglab.org>
2025-03-22 10:20:32 +0800

    fix

--- scripts/test_osspath.py
+++ scripts/test_osspath.py
@@ -9,6 +9,10 @@ from baselines.core.file_utils import read_jsonl
 if __name__ == '__main__':
     file_path = "oss://si002558te8h/dclm/process_tasks.json"
 
-    with oss.OSSPath(file_path).open("r") as f:
-        data = json.loads(f.read())
+    with open("./process_tasks.json", "w") as localf:
+        with oss.OSSPath(file_path).open("r") as f:
+            data = json.loads(f.read())
+            json.dump(data, localf, indent=4)
+    
+    
     # list(read_jsonl(file_path))
