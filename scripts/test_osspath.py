4f19d475b1ad1fc4632e1975ada8a36790b02eb9
许飞 <feixu@zhejianglab.org>
2025-03-22 08:26:19 +0800

    fix

--- scripts/test_osspath.py
+++ scripts/test_osspath.py
@@ -6,6 +6,6 @@ from baselines.core.file_utils import read_jsonl
 
 
 if __name__ == '__main__':
-    file_path = "oss://si002558te8h/dclm/output/tianwei_0319/DCLM_sub_by_keywords/processed_data/s1=10_2025030815581036g0lsuxg0sw1_R2_1_1_16_0-0_TableSink1-0-.tsv_processed.tsv"
+    file_path = "oss://si002558te8h/dclm/process_tasks.jsonle"
 
     list(read_jsonl(file_path))
