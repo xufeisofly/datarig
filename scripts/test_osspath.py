d98ed352b55c2d00df573809e8eb46e0884d402c
许飞 <feixu@zhejianglab.org>
2025-03-23 00:46:20 +0800

    fix

--- scripts/test_osspath.py
+++ scripts/test_osspath.py
@@ -3,7 +3,7 @@ import os
 import json
 
 from baselines.oss import oss
-from baselines.core.file_utils import read_jsonl
+from baselines.core.file_utils import is_exists, read_jsonl
 
 def finished_task_file(task_file_path):
     filename = os.path.basename(task_file_path)
@@ -19,10 +19,11 @@ if __name__ == '__main__':
             data = json.loads(f.read())
             json.dump(data, localf, indent=4)
 
-    with open("./finished_process_tasks.json", "w") as localf:
-        with oss.OSSPath(fin_file_path).open("r") as f:
-            data = json.loads(f.read())
-            json.dump(data, localf, indent=4)    
+    if is_exists(fin_file_path):
+        with open("./finished_process_tasks.json", "w") as localf:
+            with oss.OSSPath(fin_file_path).open("r") as f:
+                data = json.loads(f.read())
+                json.dump(data, localf, indent=4)    
     
     
     # list(read_jsonl(file_path))
