4b7fae8b723cc14fcb667e6a2a19ee8ebeaea157
许飞 <feixu@zhejianglab.org>
2025-03-27 00:30:23 +0800

    fix

--- scripts/get_nodes_memory.py
+++ scripts/get_nodes_memory.py
@@ -11,15 +11,26 @@ def get_nodes_memory():
     except subprocess.CalledProcessError as e:
         return f"Error: {e}"
 
+def get_pods_memory():
+    """调用 kubectl top nodes 获取节点内存使用情况"""
+    try:
+        output = subprocess.check_output(["kubectl", "top", "pods"], encoding="utf-8")
+        return output
+    except subprocess.CalledProcessError as e:
+        return f"Error: {e}"    
+
 def main():
     log_file = "top.log"
     while True:
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         mem_usage = get_nodes_memory()
+        pod_mem_usage = get_pods_memory()
         with open(log_file, "a", encoding="utf-8") as f:
             f.write(f"Timestamp: {timestamp}\n")
             f.write(mem_usage)
             f.write("\n")
+            f.write(pod_mem_usage)
+            f.write("\n")            
         time.sleep(10)
 
 if __name__ == '__main__':
