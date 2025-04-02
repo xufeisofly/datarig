faf0614f63f868600ac988b10dc9a53909d71e20
许飞 <feixu@zhejianglab.org>
2025-04-02 17:43:35 +0800

    fix

--- scripts/sync_quene.py
+++ scripts/sync_quene.py
@@ -13,3 +13,4 @@ if __name__ == '__main__':
     queue = TaskQueue(redis.Client, queue_id='default')
     queue.download_to_jsonl('./task_queue.jsonl')
     queue.download_processing_to_jsonl('./processing_task_queue.jsonl')
+    queue.download_finished_to_jsonl('./finished_task_queue.jsonl')
