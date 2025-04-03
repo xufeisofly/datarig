aa6d4c4d948ce8e0ac86876ab725823b3af5c80b
许飞 <feixu@zhejianglab.org>
2025-04-03 15:27:12 +0800

    fix

--- scripts/sync_queue.py
+++ scripts/sync_queue.py
@@ -2,6 +2,7 @@ import time
 import logging
 from baselines.task_queue.task_queue import TaskQueue
 from baselines.redis import redis
+from baselines.oss import oss
 from baselines.core.file_utils import is_exists, read_jsonl, write_jsonl
 
 
@@ -12,10 +13,18 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(mes
 if __name__ == '__main__':
     queue = TaskQueue(redis.Client, queue_id='default')
     while True:
+        tmp_folder = "oss://si002558te8h/dclm/temp_dir_500/"
+        bucket_name, path = oss.split_file_path(tmp_folder)
+        bucket = oss.Bucket(bucket_name)
+        files = oss.get_sub_files(bucket, path)
+        logging.info("==============")
+        logging.info("temp files: {}".format(len(files)))
+
+        
         queue.download_to_jsonl('./task_queue.jsonl')
         queue.download_processing_to_jsonl('./processing_task_queue.jsonl')
         queue.download_finished_to_jsonl('./finished_task_queue.jsonl')
-        print(f"pending: {queue.sizeof(queue.queue)}")
-        print(f"processing: {queue.sizeof(queue.processing_queue)}")
-        print(f"finished: {queue.sizeof(queue.finished_queue)}")
+        logging.info(f"pending: {queue.sizeof(queue.queue)}")
+        logging.info(f"processing: {queue.sizeof(queue.processing_queue)}")
+        logging.info(f"finished: {queue.sizeof(queue.finished_queue)}")
         time.sleep(10)
