8e8654902f9c2b4699ef49952adb3ac457583d8b
许飞 <feixu@zhejianglab.org>
2025-04-10 14:17:23 +0800

    fix

--- scripts/copy_oss_dir.py
+++ scripts/copy_oss_dir.py
@@ -1,11 +1,18 @@
 from baselines.oss import oss
 import os
+import concurrent.futures
 
 def main():
     oss_target_dir = "oss://train1/basemodel-subjet-data-processed/r2_processed/dclm/"
     oss_source_dir = "oss://si002558te8h/dclm/output/r2_formal/dclm/"
     bucket_name, target_dir = oss.split_file_path(oss_target_dir)
     target_bucket = oss.Bucket(bucket_name)
+
+
+    def process(f, target_key, source_bucket_name):
+        filename = os.path.basename(f)
+        target_bucket.copy_object(source_bucket_name, f, os.path.join(target_key, filename))
+        return target_key
 
 
     for subject in ['subject=AppliedMathematics', 'subject=AppliedPhysics', 'subject=OrganicChemistry']:
@@ -15,10 +22,16 @@ def main():
         files = oss.get_sub_files(source_bucket, source_dir)
 
         target_key = os.path.join(target_dir, subject)
-        for i, f in enumerate(files):
-            filename = os.path.basename(f)
-            target_bucket.copy_object(source_bucket_name, f, os.path.join(target_key, filename))
-            print(f"{subject} ==== {i}/{len(files)}")
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+            futures = []
+        
+            for f in files:
+                futures.append(executor.submit(process, f, target_key, source_bucket_name))
+
+            for future in concurrent.futures.as_completed(futures):
+                key = future.result()
+                print(f"done: {key}")
         
         
 if __name__ == '__main__':
