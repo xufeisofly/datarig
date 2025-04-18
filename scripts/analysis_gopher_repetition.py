7a7d318117a2307a9bee6d5d7ca18100dc4a08e7
许飞 <feixu@zhejianglab.org>
2025-04-18 16:24:18 +0800

    fix

--- scripts/analysis_gopher_repetition.py
+++ scripts/analysis_gopher_repetition.py
@@ -3,28 +3,37 @@ from baselines.mappers.filters.content_filters import *
 
 if __name__ == '__main__':
     data = """
-Multicritical points and crossover mediating the strong violation of universality: Wang-Landau determinations in the random-bond d=2 Blume-Capel model
+Ergodicity of robust switching control and nonlinear system of quasi variational inequalities
 
-Malakis, A. and Berker, A. Nihat and Hadjiagapiou, I. A. and Fytas, N. G. and Papakonstantinou, T. (2010) Multicritical points and crossover mediating the strong violation of universality: Wang-Landau determinations in the random-bond d=2 Blume-Capel model.
+Abstract : We analyze the asymptotic behavior for a system of fully nonlinear parabolic and elliptic quasi variational inequalities. These equations are related to robust switching control problems introduced in [3]. We prove that, as time horizon goes to infinity (resp. discount factor goes to zero) the long run average solution to the parabolic system (resp. the limiting discounted solution to the elliptic system) is characterized by a solution of a nonlinear system of ergodic variational inequalities. Our results hold under a dissipativity condition and without any non degeneracy assumption on the diffusion term. Our approach uses mainly probabilistic arguments and in particular a dual randomized game representation for the solution to the system of variational inequalities.
+Type de document :
+Pré-publication, Document de travail
+to appear in SIAM Journal on Control and Optimization. 2017
+Liste complète des métadonnées
+Contributeur : Huyen Pham <>
+Soumis le : vendredi 3 février 2017 - 22:13:54
+Dernière modification le : vendredi 17 février 2017 - 16:11:53
 
-Full text not available from this repository.
 
-Official URL: http://arxiv.org/abs/1001.1240
+Fichiers produits par l'(les) auteur(s)
 
 
-The effects of bond randomness on the phase diagram and critical behavior of the square lattice ferromagnetic Blume-Capel model are discussed. The system is studied in both the pure and disordered versions by the same efficient two-stage Wang-Landau method for many values of the crystal field, restricted here in the second-order phase transition regime of the pure model. For the random-bond version several disorder strengths are considered. We present phase diagram points of both pure and random versions and for a particular disorder strength we locate the emergence of the enhancement of ferromagnetic order observed in an earlier study in the ex-first-order regime. The critical properties of the pure model are contrasted and compared to those of the random model. Accepting, for the weak random version, the assumption of the double logarithmic scenario for the specific heat we attempt to estimate the range of universality between the pure and random-bond models. The behavior of the strong disorder regime is also discussed and a rather complex and yet not fully understood behavior is observed. It is pointed out that this complexity is related to the ground-state structure of the random-bond version.
+  • HAL Id : hal-01104773, version 2
+  • ARXIV : 1501.04477
 
-Item Type:Article
-Subjects:Q Science > QC Physics
-ID Code:13760
-Deposited By:A. Nihat Berker
-Deposited On:16 Feb 2010 09:13
-Last Modified:16 Feb 2010 09:13
 
-Repository Staff Only: item control page    
+Erhan Bayraktar, Andrea Cosso, Huyên Pham. Ergodicity of robust switching control and nonlinear system of quasi variational inequalities . to appear in SIAM Journal on Control and Optimization. 2017. <hal-01104773v2>
+
+
+
+Consultations de
+la notice
+
+
+Téléchargements du document    
     """
     page = {
         'text': data,
     }
-    ret = massive_web_repetition_filters(page, tokenizer='uniseg', annotate=True, token="", debug=True)
+    ret = massive_web_repetition_filters(page, tokenizer='fasttext', annotate=True, token="", debug=True)
     print(ret)
