48b9b88d641bb782c228cbcdc9544a8d52d859c0
许飞 <feixu@zhejianglab.org>
2025-04-27 16:56:38 +0800

    fix

--- scripts/all_sample.py
+++ scripts/all_sample.py
@@ -11,6 +11,8 @@ import logging
 import argparse
 from baselines.oss import oss
 from baselines.core.file_utils import is_exists, read_jsonl, write_jsonl  # 如果需要判断是否存在
+from baselines.mappers.enrichers.language_id_enrichers import *
+from baselines.mappers.filters.metadata_filters import *
 
 subjects_str = """
 Acoustics
@@ -165,34 +167,226 @@ subjects_strs = [
     "Law",
 ]
 
+
+subjects_strs = ["AnalyticalChemistry",
+ "Allergy",
+ "Anesthesiology",
+ "AnatomyMorphology",
+ "Acoustics",
+ "Architecture",
+ "ArtPracticeHistoryTheory",
+ "ArthritisRheumatology",
+ "Anthropology",
+ "ArtificialIntelligenceImageProcessing",
+ "AppliedMathematics",
+ "BehavioralScienceComparativePsychology",
+ "BiochemistryMolecularBiology",
+ "AgriculturalEconomicsPolicy",
+ "Archaeology",
+ "Biophysics",
+ "AerospaceAeronautics",
+ "BuildingConstruction",
+ "Accounting",
+ "CardiovascularSystemHematology",
+ "AutomobileDesignEngineering",
+ "Bioinformatics",
+ "ChemicalEngineering",
+ "ChemicalPhysics",
+ "ClinicalPsychology",
+ "AstronomyAstrophysics",
+ "ComplementaryAlternativeMedicine",
+ "ComputationTheoryMathematics",
+ "AppliedEthics",
+ "ComputerHardwareArchitecture",
+ "CommunicationMediaStudies",
+ "BiomedicalEngineering",
+ "AgronomyAgriculture",
+ "AppliedPhysics",
+ "DermatologyVenerealDiseases",
+ "DesignPracticeManagement",
+ "Dentistry",
+ "DairyAnimalScience",
+ "DevelopmentalChildPsychology",
+ "DistributedComputing",
+ "DevelopmentalBiology",
+ "DramaTheater",
+ "Classics",
+ "EconomicTheory",
+ "Econometrics",
+ "Ecology",
+ "Criminology",
+ "EmergencyCriticalCareMedicine",
+ "EndocrinologyMetabolism",
+ "Education",
+ "CivilEngineering",
+ "Entomology",
+ "EnvironmentalOccupationalHealth",
+ "Demography",
+ "Epidemiology",
+ "ElectricalElectronicEngineering",
+ "ExperimentalPsychology",
+ "EvolutionaryBiology",
+ "EnvironmentalEngineering",
+ "Biotechnology",
+ "CulturalStudies",
+ "Folklore",
+ "FluidsPlasmas",
+ "FamilyStudies",
+ "GastroenterologyHepatology",
+ "Energy",
+ "Finance",
+ "GeneralChemistry",
+ "GeneralClinicalMedicine",
+ "GeneralInternalMedicine",
+ "Fisheries",
+ "GeneralPsychologyCognitiveSciences",
+ "GeneticsHereditary",
+ "FoodScience",
+ "GeochemistryGeophysics",
+ "GenderStudies",
+ "Forestry",
+ "Geriatrics",
+ "EnvironmentalSciences",
+ "Gerontology",
+ "GeneralMathematics",
+ "HistoryofScienceTechnologyMedicine",
+ "History",
+ "HealthPolicyServices",
+ "HumanFactors",
+ "GeologicalGeomaticsEngineering",
+ "Immunology",
+ "Geography",
+ "HistoryofSocialSciences",
+ "InformationSystems",
+ "InorganicNuclearChemistry",
+ "IndustrialEngineeringAutomation",
+ "Geology",
+ "IndustrialRelations",
+ "LegalForensicMedicine",
+ "LanguagesLinguistics",
+ "Law",
+ "MarineBiologyHydrobiology",
+ "LogisticsTransportation",
+ "InformationLibrarySciences",
+ "MathematicalPhysics",
+ "GeneralPhysics",
+ "MedicalInformatics",
+ "MedicinalBiomolecularChemistry",
+ "Horticulture",
+ "Microbiology",
+ "Microscopy",
+ "MeteorologyAtmosphericSciences",
+ "Music",
+ "MycologyParasitology",
+ "MiningMetallurgy",
+ "NetworkingTelecommunications",
+ "NeurologyNeurosurgery",
+ "NuclearMedicineMedicalImaging",
+ "NanoscienceNanotechnology",
+ "Marketing",
+ "NumericalComputationalMathematics",
+ "LiteraryStudies",
+ "NutritionDietetics",
+ "ObstetricsReproductiveMedicine",
+ "OncologyCarcinogenesis",
+ "NuclearParticlePhysics",
+ "OphthalmologyOptometry",
+ "OperationsResearch",
+ "Materials",
+ "OrganicChemistry",
+ "InternationalRelations",
+ "Orthopedics",
+ "Otorhinolaryngology",
+ "Optics",
+ "Pathology",
+ "Pediatrics",
+ "PharmacologyPharmacy",
+ "Nursing",
+ "PhysicalChemistry",
+ "Physiology",
+ "Ornithology",
+ "MechanicalEngineeringTransports",
+ "Polymers",
+ "Psychiatry",
+ "Psychoanalysis",
+ "OptoelectronicsPhotonics",
+ "Oceanography",
+ "DevelopmentStudies",
+ "RespiratorySystem",
+ "ScienceStudies",
+ "SocialPsychology",
+ "Paleontology",
+ "SocialSciencesMethods",
+ "Rehabilitation",
+ "SoftwareEngineering",
+ "PlantBiologyBotany",
+ "PublicHealth",
+ "SportSciences",
+ "Philosophy",
+ "SpeechLanguagePathologyAudiology",
+ "SocialWork",
+ "Surgery",
+ "Toxicology",
+ "TropicalMedicine",
+ "SubstanceAbuse",
+ "UrologyNephrology",
+ "UrbanRegionalPlanning",
+ "Virology",
+ "BusinessManagement",
+ "PoliticalSciencePublicAdministration",
+ "SportLeisureTourism",
+ "StatisticsProbability",
+ "Zoology",
+ "Sociology",
+ "VeterinarySciences",
+ "StrategicDefenceSecurityStudies",
+ "Economics",
+ "ReligionsTheology"]
+
 # 设置日志
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
 
 
-def main(subject_str):
+def main(subject_str, lang='en'):
     oss_dir = "oss://train1/basemodel-subjet-data/r2/dclm/"
     bucket_name, path = oss.split_file_path(oss_dir)
     bucket = oss.Bucket(bucket_name)
 
-    num = 3000
+    num = 20
     lines = []
     subject_paths = oss.get_sub_folders(bucket, path)
+
+    lang_fn = detect_lang_whole_page_enricher(model='fasttext')
+    
     for k, subject_path in enumerate(subject_paths):
         subject_name = subject_path.split("/")[-2].split("=")[-1]
         if subject_name not in subject_str:
             continue
 
-        print(k)
+        
         files = oss.get_sub_files(bucket, subject_path)
         f = files[0]
         f = oss.join_file_path(bucket_name, f)
+        print(f"{subject_path}, {f}")
         for i, line in enumerate(read_jsonl(f)):
-            lines.append(line)
-            if i == num-1:
-                break
+            if len(line['text']) == 0:
+                continue
 
-    output_folder = "oss://si002558te8h/dclm/origin/Experiment3/"    
-    write_jsonl(lines, os.path.join(output_folder, f"{subject_str}_{num}.jsonl"))
+            line = lang_fn(line)[0]
+            rets = language_filter(line, keep_languages=[lang], key='language_id_whole_page_fasttext', threshold=0.65)
+            if not rets:
+                continue
+            line = rets[0]
+            lines.append(line)
+            
+            if len(lines) == num:
+                break
+        if len(lines) == num:
+            break
+
+    print(f"{subject_str} {lang} done {num}")
+    output_folder = "oss://si002558te8h/dclm/origin/Experiment4_en/"    
+    write_jsonl(lines, os.path.join(output_folder, f"{subject_str}_{lang}_{num}.jsonl"))
 
 if __name__ == '__main__':
     for subject_str in subjects_strs:
