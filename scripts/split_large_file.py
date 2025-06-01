c0800ea1d2256dba5b236c7f24c32f4939690635
许飞 <feixu@zhejianglab.org>
2025-06-01 11:04:10 +0800

    fix

--- scripts/split_large_file.py
+++ scripts/split_large_file.py
@@ -5,52 +5,15 @@ import concurrent.futures
 
         
 if __name__ == '__main__':
-    # oss_dir = "oss://si002558te8h/dclm/output/zhanghong/"
-    # target_oss_dir = "oss://si002558te8h/dclm/output/r3_math_2b/"
-    # bucket_name, dir_path = oss.split_file_path(oss_dir)
-    # bucket = oss.Bucket(bucket_name)
-
-    # sub_files = oss.get_sub_files(bucket, dir_path)
-
-    # files = []
-    # for sub_file in sub_files:
-    #     filename = os.path.basename(sub_file)
-    #     filename = os.path.splitext(filename)[0]
-    #     # num = int(filename.split('_')[-1])
-
-    #     # if num <= 14:
-    #     #     files.append(sub_file)
-    #     if 'Mathematics' in filename:
-    #         files.append(sub_file)
-
-    # bucket_name, target_dir = oss.split_file_path(target_oss_dir)
-    
-    
-    # with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-    #     futures = []
-        
-    #     for f in files:
-    #         futures.append(executor.submit(process, f, target_dir, bucket_name, bucket))
-
-    #         for future in concurrent.futures.as_completed(futures):
-    #             key = future.result()
-    #             print(f"done: {key}")
-
-
-    folder_path = '/root/dataprocess_nas/zh/removed/'
+    folder_path = '/root/dataprocess_nas/dataprocess/public/fineweb/fineweb-sample-10BT/sample_jsonl/'
     all_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 
     def process(file_path):
         filename = os.path.basename(file_path)
         filename = os.path.splitext(filename)[0]
-        if filename == 'Mathematics_3000000' or filename == 'Engineering_3000000':
-            return
-        target_folder = os.path.join('/root/dataprocess_nas/xf/data/low_quality/', filename.split('_')[0])
+        target_folder = os.path.join('/root/dataprocess_nas/xf/data/fineweb_10b/', filename.split('_')[0])
         os.makedirs(target_folder, exist_ok=True)
 
-        if len(os.listdir(target_folder)) == 101:
-            return
-        
         print(f"==== start {filename}")
                 
         lines = []
@@ -67,7 +30,7 @@ if __name__ == '__main__':
         print(f"==== done {filename}")        
     
 
-    with concurrent.futures.ProcessPoolExecutor(max_workers=3) as executor:
+    with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
         futures = []
         
         for file_path in all_files:
