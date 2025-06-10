7c4428db2cbd1bc048976ac5f8e4d5c736e0cded
许飞 <feixu@zhejianglab.org>
2025-06-10 14:52:23 +0800

    fix

--- baselines/mappers/filters/content_filters.py
+++ baselines/mappers/filters/content_filters.py
@@ -781,9 +781,9 @@ def fineweb_gopher_repetition_filter(
     text = page[CONTENT]
     paragraph_exp = re.compile(r"\n{2,}")
 
-    print("======= start", page['metadata']['warc_record_id'])
+    print("======= start", page['warc_record_id'])
     paragraphs = paragraph_exp.split(text.strip())
-    print("======= end", page['metadata']['warc_record_id'])
+    print("======= end", page['warc_record_id'])
     paragraphs_duplicates, char_duplicates = find_duplicates(paragraphs)
     if dup_para_frac and paragraphs_duplicates / len(paragraphs) > dup_para_frac:
         return set_filter_reason_if_annotate(page, "massive_web_repetition_filters:paragraph", annotate)
