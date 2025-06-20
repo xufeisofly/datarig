85c4e99d7cec423b89e54f71ab5d10225e97e008
许飞 <feixu@zhejianglab.org>
2025-06-20 17:21:35 +0800

    fix

--- baselines/mappers/filters/content_filters.py
+++ baselines/mappers/filters/content_filters.py
@@ -720,13 +720,7 @@ def fineweb_quality_filter(
         
     ratio = sum(1 for line in lines if line.endswith(stop_chars)) / len(lines)
     if ratio < line_punct_thr and not (ratio == 0 and line_punct_exclude_zero):
-        if high_quality_ratio(
-                lines,
-                model=model,
-                high_quality_min_line_num=high_quality_min_line_num,
-                language=language,
-        ) < high_quality_ratio_value:
-            return set_filter_reason_if_annotate(page, "line_punct_ratio_filter"+token, annotate)
+        return set_filter_reason_if_annotate(page, "line_punct_ratio_filter"+token, annotate)
 
     ratio = sum(1 for line in lines if len(line) <= short_line_length) / len(lines)
     if ratio > short_line_thr:
