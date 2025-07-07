bbff75f69c8abcadb913d1930adbb0afee12e32f
许飞 <feixu@zhejianglab.org>
2025-07-07 18:15:20 +0800

    fix

--- rust_processing/quality-rs/src/main.rs
+++ rust_processing/quality-rs/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::io::Result;
+use std::path::Path;
+use std::path::PathBuf;
+use std::thread::available_parallelism;
+
+/*======================================================
+=                              ARGS                    =
+======================================================*/
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(required = true, long)]
+    input: Vec<PathBuf>,
+
+    #[arg(required = true, long)]
+    output: PathBuf,
+
+    #[arg(long, default_value_t = 0)]
+    threads: usize,
+}
+
+/*==============================================================
+=                         MAIN BLOCK                           =
+==============================================================*/
+
+fn main() -> Result<()> {
+    env_logger::init();
+    println!("Hello");
+
+    let _args = Args::parse();
+
+    let threads = if args.threads == 0 {
+        available_parallelism().unwrap().get()
+    } else {
+        args.threads
+    };
+
+    println!("------- {:?}", threads);
+    Ok(())
 }
